4cc322eb1971ae28bf65cbe2037972f38429c8e7
jgolden83 <55173227+jgolden83@users.noreply.github.com>
2023-11-07 13:10:01 -0500

    2023.11.07 website update
    updated content

--- javascript/sp5javascrypt.js
+++ javascript/sp5javascrypt.js
@@ -1,27 +1,27 @@
 <script>
 function selectpage(i) 
 {
-    let var a = "some_string";
-    if(i == 0) 
-    {
-        a = "welcome.html"
-    }
-    if(i == 1) 
-    {
-        a = "../html/team.html"
-    }
-    if(i == 2) 
-    {
-        a = "../html/documents.html"
-    }
-    if(i == 3) 
-    {
-        a = "https://github.com/DynamicGroceryListGroup"
-    }
-    if(i == 4) 
-    {
-        a = "../html/references.html"
-    }
-    document.getElementById('page_viewer').src = a;
+   var a;
+   if(i == 0) 
+   {
+       a = "./html/welcome.html"
+   }
+   if(i == 1) 
+   {
+       a = "./html/team.html"
+   }
+   if(i == 2) 
+   {
+       a = "./html/documents.html"
+   }
+   if(i == 3) 
+   {
+       a = "https://github.com/DynamicGroceryListGroup"
+   }
+   if(i == 4) 
+   {
+       a = "./html/references.html"
+   }
+   document.getElementById('page_viewer').src = a;
 }
 </script>
